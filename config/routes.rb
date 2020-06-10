d9106719c1d254e09dd03f4ca9696504b71c7bc5
Nancy Schrader <katelanschrader7@gmail.com>
2020-06-10 18:09:14 -0400

    added react

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :events
+  namespace :api do
+      resources :events, only: [:index, :create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
+    root to: 'home#index'
 end
